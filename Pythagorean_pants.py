ad6bfcb311dcee16f13c86c2fe5efbe8fde0455e
Yurii <yurazosia@gmail.com>
2024-07-31 20:54:04 +0300

    Додаємо логування й обробку помилок
    Додаємо логування й обробку помилок, Конфігурація логування і т.д.

--- Pythagorean_pants.py
+++ Pythagorean_pants.py
@@ -1,1 +1,22 @@
 # Yurii Zosia
+import logging
+
+# Конфігурація логування
+#Можливі значення: DEBUG, INFO, WARNING, ERROR, CRITICAL
+logging.basicConfig(filename='Pythagorean_pants.log', level=logging.DEBUG)
+logging.info('Програма запущена!')
+
+# Використання логування
+#logging.debug('Це повідомлення для відлагодження')
+#logging.info('Інформаційне повідомлення')
+#logging.warning('Попередження')
+#logging.error('Помилка')
+#ogging.critical('Критична помилка')
+
+try:
+    print("Вітання Юрій!")
+    x = int(input("Введіть число: "))
+    print(10 / x)
+except Exception as e:
+    logging.error(f"Виникла помилка: {str(e)}")
+    print("Сталася помилка. Дивіться лог для деталей.")
